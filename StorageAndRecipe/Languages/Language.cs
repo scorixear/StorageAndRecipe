d49ac5a30d638d3b2cdc293ead8ad5ac085b8fdb
Paul Keller <pkeller1@stud.hs-heilbronn.de>
2019-08-06 13:08:08 +0200

    Added Language Pack

---
+++ StorageAndRecipe/Languages/Language.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace StorageAndRecipe
+{
+    [Serializable]
+    public class Language
+    {
+        public string WindowTitle { get; set; }
+        public string RecipeButton { get; set; }
+        public string StorageButton { get; set; }
+        public string LanguageMenu { get; set; }
+        public string LanguageName { get; set; }
+
+        public void Save(string file)
+        {
+            if (!Directory.Exists("Languages"))
+                Directory.CreateDirectory("Languages");
+            using(var writer = new StreamWriter("Languages/"+file))
+            {
+                var serializer = new XmlSerializer(this.GetType());
+                serializer.Serialize(writer, this);
+                writer.Flush();
+            }
+        }
+
+        public static Language Load(string file)
+        {
+            using (var stream = File.OpenRead(file))
+            {
+                var serializer = new XmlSerializer(typeof(Language));
+                return serializer.Deserialize(stream) as Language;
+            }
+        }
+    }
+}
--- StorageAndRecipe/MainWindow.xaml.cs
+++ StorageAndRecipe/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace StorageAndRecipe
 {
@@ -20,9 +10,23 @@ namespace StorageAndRecipe
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Language> languages;
         public MainWindow()
         {
+            languages = new List<Language>();
+            string[] paths = Directory.GetFiles("Languages");
+            foreach(string file in paths)
+            {
+                languages.Add(StorageAndRecipe.Language.Load(file));
+            }
             InitializeComponent();
+            LanguageMenuItem.Items.Clear();
+            foreach (Language l in languages)
+            {
+                RadioButton rb = new RadioButton();
+                rb.Content = l.LanguageName;
+                LanguageMenuItem.Items.Add(rb);
+            }
         }
     }
 }
