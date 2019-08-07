9cf8826e0fdf2e1a01c503f8cc4442f4f2965b8e
Paul Keller <pkeller1@stud.hs-heilbronn.de>
2019-08-07 23:02:58 +0200

    added language change functionality

--- StorageAndRecipe/App.xaml.cs
+++ StorageAndRecipe/App.xaml.cs
@@ -13,5 +13,6 @@ namespace StorageAndRecipe
     /// </summary>
     public partial class App : Application
     {
+        
     }
 }
--- StorageAndRecipe/MainWindow.xaml.cs
+++ StorageAndRecipe/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,7 +18,10 @@ namespace StorageAndRecipe
             string[] paths = Directory.GetFiles("Languages");
             foreach(string file in paths)
             {
-                languages.Add(StorageAndRecipe.Language.Load(file));
+                Language l = StorageAndRecipe.Language.Load(file);
+                string[] s = file.Substring(0,file.Length-4).Split("\\".ToCharArray());
+                l.LanguageName = s[s.Length - 1];
+                languages.Add(l);
             }
             InitializeComponent();
             LanguageMenuItem.Items.Clear();
@@ -25,8 +29,45 @@ namespace StorageAndRecipe
             {
                 RadioButton rb = new RadioButton();
                 rb.Content = l.LanguageName;
+                rb.Click += LanguageRadioButtonClick;
                 LanguageMenuItem.Items.Add(rb);
             }
+        }
+
+        private void LanguageRadioButtonClick(object sender, RoutedEventArgs e)
+        {
+            RadioButton senderRB = (RadioButton)sender;
+            foreach(RadioButton rb in LanguageMenuItem.Items)
+            {
+                if(rb.Equals(senderRB) ==false)
+                {
+                    rb.IsChecked = false;
+                }
+            }
+            senderRB.IsChecked = true;
+            foreach(Language l in languages)
+            {
+                if(l.LanguageName == (string)senderRB.Content)
+                {
+                    SetLanguage(l);
+                    return;
+                }
+            }
+            throw new Exception("Language not found");
+
+        }
+
+        private void SetLanguage(Language l)
+        {
+            this.Title = l.WindowTitle;
+            StorageButton.Content = l.StorageButton;
+            RecipeButton.Content = l.RecipeButton;
+            LanguageMenuItem.Header = l.LanguageMenu;
+        }
+
+        private void StorageButton_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
